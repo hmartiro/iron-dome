0390250d06425796fd7e7035410683ce7466fc94
Hayk Martirosyan <hayk.mart@gmail.com>
2014-11-21 21:43:44 -0800

    Projectile generator successfully sending to vision thread

--- src/projectile/projectile_test.cpp
+++ src/projectile/projectile_test.cpp
@@ -11,6 +11,8 @@
 
 using namespace std;
 
+const string ADDRESS = "tcp://*:4242";
+
 int main(int argc, char* argv[]) {
 
   // Projectile generation parameters
@@ -28,6 +30,14 @@ int main(int argc, char* argv[]) {
   // Set print format
   cout << fixed << setprecision(3);
 
+  // Initialize a 0MQ publish socket
+  zmqpp::context context;
+  zmqpp::socket socket (context, zmqpp::socket_type::publish);
+
+  // Open the connection
+  cout << "Binding to " << ADDRESS << "..." << endl;
+  socket.bind(ADDRESS);
+
   // Loop through, generating projectiles
   double t = sutil::CSystemClock::getSysTime();
   while(t < tEnd) {
@@ -36,7 +46,22 @@ int main(int argc, char* argv[]) {
     pg.update();
 
     for(const pair<int, SimProjectile>& p : pg.getProjectiles()) {
+
       const SimProjectile& proj = p.second;
+      zmqpp::message message;
+//      stringstream ss;
+//      ss << " hello: ";// << proj.p.transpose();
+//      cout << "Message: " << ss << endl;
+//              << proj.p(0) << " "
+//              << proj.p(1) << " "
+//              << proj.p(2);
+      message << to_string(proj.id) + " " + to_string(t) + " "
+               + to_string(proj.p(0)) + " "
+               + to_string(proj.p(1)) + " "
+               + to_string(proj.p(2));
+      
+      socket.send(message);
+
       cout << "Projectile " << proj.id << ", t = " << t << ", position = ("
            << proj.p(0) << ", "
            << proj.p(1)  << ", "
